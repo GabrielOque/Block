8a4b29a4914013e315676a9755c6a26446bd9b4d
Gabriel Oquendo <oquendogabriel18@gamil.com>
2022-11-01 18:10:47 -0500

    Creamos es header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
-
-function App() {
+import buscar from "./buscar.png";
+const App = () => {
   return (
     <>
-      <div>hola </div>
-      <p>eyyyy</p>
-      <p>eyyyy</p>
-      <h>este es un titulo</h>
+      <div className="h-screen w- full">
+        <div className="flex w-full h-20 bg-gray-800">
+          <div className="flex w-2/3 h-5 mt-5 font-bold text-white cursor-pointer justify-evenly">
+            <div>Franquicias</div>
+            <div>Fiscal y legal</div>
+            <div>Ecommerces</div>
+            <div>Sostenibilidad</div>
+            <div>Tienda</div>
+          </div>
+          <div className="flex w-1/3 h-5 mt-5 font-bold text-white cursor-pointer justify-evenly">
+            <div>Suscribete</div>
+            <div>Notiias</div>
+            <div>
+              <img className="w-10 h-10" src={buscar} />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default App;
