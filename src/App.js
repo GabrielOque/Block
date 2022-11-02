189f9727f7f898c4588786897c4fe5f28d582b17
Gabriel Oquendo <oquendogabriel18@gamil.com>
2022-11-02 00:36:45 -0500

    cambios en la fuente

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <>
       <div className="h-screen w- full">
-        <div className="flex w-full h-20 bg-gray-800">
+        <div className="flex w-full h-20 text-2xl bg-gray-800">
           <div className="flex w-2/3 h-5 mt-5 font-bold text-white cursor-pointer justify-evenly">
             <div>Franquicias</div>
             <div>Fiscal y legal</div>
