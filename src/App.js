2967cc1a111632f11c1cc424bdbcf128f32f972d
Sebastian-Monsalve <sebastianmonsalvec16@gmail.com>
2022-11-03 19:44:48 -0500

    Nuevos cambio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import buscar from "./buscar.png";
 const App = () => {
   return (
     <>
-      <div className="h-screen w- full">
+      <div className="h-screen w- full text-align:center">
         <div className="flex w-full h-20 text-2xl bg-gray-800">
           <div className="flex w-2/3 h-5 mt-5 font-bold text-white cursor-pointer justify-evenly">
             <div>Franquicias</div>
@@ -14,18 +14,16 @@ const App = () => {
           </div>
           <div className="flex w-1/3 h-5 mt-5 font-bold text-white cursor-pointer justify-evenly">
             <div>Suscribete</div>
-            <div>Notiias</div>
+            <div>Noticias</div>
             <div>
               <img className="w-10 h-10" src={buscar} alt="error" />
             </div>
           </div>
         </div>
-        <h>HOLA PUTITO, soy sebas he hice cambios</h>
-        <div className="w-30 h-50 text-3xl font-bold text-white bg-blue-700">
-          Este es mi aporte
-        </div>
-        <div className="w=50 h-30 text-orange 300 bg-green-800 mt-6">
-          soy pendejo
+        <h>¡Welcome to best block of fucking world!</h>
+        <h1>Today, the friend Gabriel not ate his lunch </h1>
+        <div className="flex w-20 h-30 fond-bold text-white bg-green-800 mt-6 ml-8 cursor-pointer">
+          CLIC FOR SUSCRIBE
         </div>
       </div>
     </>
